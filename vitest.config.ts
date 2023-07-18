e53ba4b10edfa5ca1039f6bb67e1c9f250d54c5b
Adroaldo de Andrade <adroaldof@gmail.com>
2023-07-17 23:12:04 -0300

    chore(typescript): add paths

---
+++ vitest.config.ts
@@ -0,0 +1,24 @@
+import path from 'path'
+import { defineConfig } from 'vitest/config'
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [],
+  test: {
+    coverage: {
+      reporter: ['text', 'text-summary', 'html'],
+      skipFull: true,
+    },
+    testTimeout: 5000,
+  },
+  resolve: {
+    alias: {
+      '@/ports': path.resolve(__dirname, './src/business/application/ports'),
+      '@/use-cases': path.resolve(__dirname, './src/business/application/use-cases'),
+      '@/entities': path.resolve(__dirname, './src/business/domain/entities'),
+      '@/': path.resolve(__dirname, './src'),
+    },
+  },
+})
