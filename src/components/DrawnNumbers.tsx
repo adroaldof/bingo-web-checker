637185a01007dc66adc39f805632974cadee3d0e
Adroaldo de Andrade <adroaldof@gmail.com>
2023-07-26 20:22:12 -0300

    feat(drawn-numbers): add last drawn numbers

---
+++ src/components/DrawnNumbers.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+type DrawnNumbersProps = {
+  drawnNumbers: number[]
+}
+
+export const DrawnNumbers = ({ drawnNumbers }: DrawnNumbersProps) => {
+  return (
+    <div className="flex flex-col w-full">
+      <h3>Números sorteados</h3>
+      <div data-cy="drawn-numbers" className="grid grid-cols-10 gap-1 p-1 grid-rows-10 bg-slate-200">
+        {Array.from(Array(90).keys()).map((number) => {
+          const gridNumber = number + 1
+          const isDrawn = drawnNumbers.includes(+gridNumber)
+          const backgroundColor = isDrawn ? 'green' : 'gray'
+          const textColor = isDrawn ? 'green' : 'gray'
+          return (
+            <div
+              key={gridNumber}
+              data-cy={`drawn-${gridNumber}`}
+              className={`bg-${backgroundColor}-300 p-1 py-0 flex flex-1 justify-center rounded-sm text-${textColor}-700`}
+            >
+              {gridNumber < 10 ? `0${gridNumber}` : gridNumber}
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+const sortAscending = (firstNumber: number, secondNumber: number) => firstNumber - secondNumber
